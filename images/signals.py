2b0ae844d0363cdbea367be5358dd1671465967e
dmitriy <dmleagy@gmail.com>
2025-11-26 17:41:32 +0300

    Fix signal

--- images/signals.py
+++ images/signals.py
@@ -6,5 +6,5 @@ from .models import Image
 
 @receiver(m2m_changed, sender=Image.users_like.through)
 def users_like_changed(sender, instance, action, **kwargs):
-    instance.total_likes = instance.likes.count()
+    instance.total_likes = instance.users_like.count()
     instance.save()
